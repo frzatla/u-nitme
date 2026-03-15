9b0947e54551f6ce97936ac8f66878478d5d38b2
firza pradipta atalla <pradipta.firza@gmail.com>
2026-03-15 13:35:07 +1100

    Finished creation of the ndjson files for Elastic integration.

--- my-app/public/data/cleanup.js
+++ my-app/public/data/cleanup.js
@@ -1,20 +1,17 @@
 const fs = require("node:fs")
 
-const INPUT_FILE = "final_aos.json"
+const INPUT_FILE = "final_units.json"
 const OUTPUT_FILE = INPUT_FILE.replace(".json", ".ndjson")
 
 const data = Object.values(JSON.parse(fs.readFileSync(INPUT_FILE, "utf-8")))
 
-data.map(aos => {
-    delete aos.all_units;
-    aos.requirement_groups.map(group => {
-        delete group.parent_id;
-        delete group.num_required;
-    })
+data.map(obj => {
+    delete obj.academic_org;
+    delete obj.sca_band;
 })
 
 function createNDJSON(filename, data) {
-    fs.writeFileSync(filename, data.map(it => JSON.stringify(it)).join("\n"))
+    fs.writeFileSync(filename, data.map(it => JSON.stringify(it)).join("\n") + "\n")
 }
 
 createNDJSON(OUTPUT_FILE, data)
