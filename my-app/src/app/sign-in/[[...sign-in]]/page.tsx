cbd182f9371696f12a05cec3deee9721cccf899f
firza pradipta atalla <pradipta.firza@gmail.com>
2026-05-15 16:59:01 +1000

    Centered log-in screen

--- my-app/src/app/sign-in/[[...sign-in]]/page.tsx
+++ my-app/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -2,9 +2,11 @@ import { SignIn } from "@clerk/nextjs";
 
 export default async function Page() {
   return (
-    <SignIn
-      forceRedirectUrl="/post-sign-in"
-      fallbackRedirectUrl="/post-sign-in"
-    />
+    <main className="flex min-h-screen items-center justify-center bg-black px-4">
+      <SignIn
+        forceRedirectUrl="/post-sign-in"
+        fallbackRedirectUrl="/post-sign-in"
+      />
+    </main>
   );
 }
